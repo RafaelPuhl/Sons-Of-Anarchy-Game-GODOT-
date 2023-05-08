3972bc3711e4fb4ff093fa88feac32f84e4480cb
Rafael Dias Puhl <rafaeldiaspuhl@gmail.com>
2023-05-08 18:10:56 -0300

    atualizando

--- Player/Player_JaxTeller.gd
+++ Player/Player_JaxTeller.gd
@@ -14,12 +14,17 @@ func _physics_process(delta):
 	
 	var direcao = Input.get_action_strength("right") - Input.get_action_strength("left")
 	velocidade.x = direcao * 600
-	
-	if velocidade.x > 0:
-		sprite.play("right")
-	elif Input.get_action_strength("atirar"):
+		
+	if velocidade.x > 0 and Input.get_action_strength("atirar"):
 		sprite.play("fire")
 		print("Atirou")
+		
+	elif Input.get_action_strength("atirar"):
+		sprite.play("fire")
+		
+	elif velocidade.x > 0:
+		sprite.play("right")
+		
 	else:
 		sprite.stop()
 		sprite.frame = 0
