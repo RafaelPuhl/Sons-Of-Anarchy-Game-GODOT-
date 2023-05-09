c96708810fa367c36c3880f4fe824c831cb90dad
Rafael Dias Puhl <rafaeldiaspuhl@gmail.com>
2023-05-09 20:14:37 -0300

    adicionado sons e criado fisica de parada por inercia

--- Player/Player_JaxTeller.gd
+++ Player/Player_JaxTeller.gd
@@ -2,35 +2,50 @@ extends KinematicBody2D
 
 const GRAVIDADE = 2500
 var velocidade = Vector2.ZERO
+var max_speed = 600
+var desaceleration_rate = 200
+var desaceleration_timer = 0.0
 
 onready var sprite = $AnimatedSprite
+onready var shootSound = $ShootSound
 
 func _ready() -> void:
 	pass
 
 func _physics_process(delta):
 	velocidade.y += GRAVIDADE * delta
-	velocidade = move_and_slide(velocidade)
-	
+	velocidade = move_and_slide(velocidade,Vector2.UP)
+
 	var direcao = Input.get_action_strength("right") - Input.get_action_strength("left")
-	velocidade.x = direcao * 600
-		
+
+	if direcao != 0:
+		velocidade.x = direcao * max_speed
+		desaceleration_timer = 0
+	elif Input.is_action_just_pressed("stop"):
+		velocidade.x = 0
+	else:
+		desaceleration_timer += delta
+		if desaceleration_timer < 2:
+			velocidade.x -= desaceleration_rate * delta * sign(velocidade.x)
+			velocidade.x = clamp(velocidade.x, -max_speed, max_speed)
+		else:
+			velocidade.x = 0
+
 	if velocidade.x > 0 and Input.get_action_strength("atirar"):
 		sprite.play("fire")
-		print("Atirou")
-		
+		shootSound.play()
+
 	elif Input.get_action_strength("atirar"):
 		sprite.play("fire")
-		
+		shootSound.play()
+
 	elif velocidade.x > 0:
 		sprite.play("right")
-		
+
 	else:
 		sprite.stop()
 		sprite.frame = 0
-		
-	if Input.is_action_just_pressed("jump"):
+
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		print("Pulou")
 		velocidade.y = -1000
-		
-	
