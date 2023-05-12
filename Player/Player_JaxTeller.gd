deef05ce3ff5901c472a0df7de97ed03417c35cb
Rafael Dias Puhl <rafaeldiaspuhl@gmail.com>
2023-05-12 18:52:41 -0300

    validacao_termino_sprite_para_atirar

--- Player/Player_JaxTeller.gd
+++ Player/Player_JaxTeller.gd
@@ -5,6 +5,7 @@ var velocidade = Vector2.ZERO
 var max_speed = 600
 var desaceleration_rate = 200
 var desaceleration_timer = 0.0
+var shooting = false
 
 onready var sprite = $AnimatedSprite
 onready var shootSound = $ShootSound
@@ -16,7 +17,7 @@ func _physics_process(delta):
 	velocidade.y += GRAVIDADE * delta
 	velocidade = move_and_slide(velocidade,Vector2.UP)
 
-	var direcao = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var direcao = Input.get_action_strength("right")
 
 	if direcao != 0:
 		velocidade.x = direcao * max_speed
@@ -33,11 +34,10 @@ func _physics_process(delta):
 
 	if velocidade.x > 0 and Input.get_action_strength("atirar"):
 		sprite.play("fire")
-		shootSound.play()
 
 	elif Input.get_action_strength("atirar"):
 		sprite.play("fire")
-		shootSound.play()
+		
 
 	elif velocidade.x > 0:
 		sprite.play("right")
@@ -49,3 +49,14 @@ func _physics_process(delta):
 	if is_on_floor() and Input.is_action_just_pressed("jump"):
 		print("Pulou")
 		velocidade.y = -1000
+		
+	if shooting == true:
+		shootSound.play()
+		shooting = false
+
+func _on_AnimatedSprite_animation_finished() -> void:
+	print("Terminou")
+	print(shooting)
+	if sprite.animation == "fire":
+		shooting = true
+		print(shooting)
