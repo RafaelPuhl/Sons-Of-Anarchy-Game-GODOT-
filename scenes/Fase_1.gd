8ae043f637b20e499d3ae55bab011b57e472ee48
Rafael Dias Puhl <rafaeldiaspuhl@gmail.com>
2023-06-12 18:39:55 -0300

    more_enemies

--- scenes/Fase_1.gd
+++ scenes/Fase_1.gd
@@ -1,25 +1,37 @@
 extends Node2D
 
-var Enemy = preload("res://Enemies/Enemy_Fear.tscn")
+var EnemyFear = preload("res://Enemies/Enemy_Fear.tscn")
+var EnemyA = preload("res://Enemies/Enemy_A.tscn")
 onready var enemiesTimer := $Timer_enemies as Timer
 onready var player := $Player_JaxTeller
 var enemiesPosition = 0
 var sceneLimit : Position2D
 var timer
+var random = RandomNumberGenerator.new()
 
 func _ready() -> void:
 	sceneLimit = get_node("scene_limit")
 	player = get_node("Player_JaxTeller")
 
-func newEnemy():
-	var e1 = Enemy.instance()
+func newEnemyFear():
+	var e1 = EnemyFear.instance()
 	enemiesPosition = player.global_position
 	e1.position = Vector2(enemiesPosition.x + 2900, 900) # Ajuste de acordo com as suas necessidades
 	add_child(e1)
+	
+func newEnemyA():
+	var eA = EnemyA.instance()
+	enemiesPosition = player.global_position
+	eA.position = Vector2(enemiesPosition.x + 2900, 900) # Ajuste de acordo com as suas necessidades
+	add_child(eA)
 
 func _on_Timer_timeout() -> void:
 	if player.position.x < sceneLimit.position.x:
-		newEnemy()
+		random.randomize()
+		if random.randi_range(1, 2) == 1:
+			newEnemyFear()
+		elif random.randi_range(1, 2) == 2:
+			newEnemyA()
 	
 func _physics_process(delta: float) -> void:
 	
